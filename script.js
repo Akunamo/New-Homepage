a06744c1028d6d48c8c60ea6c8783e852790c640
Aviral <2knightsoldier@gmail.com>
2022-11-27 19:21:30 +0530

    responsive-nav complete

---
+++ script.js
@@ -0,0 +1,14 @@
+const navigation = document.getElementById('navigation');
+const navToggleBtn = document.querySelector('.toggle-nav')
+
+
+
+
+navToggleBtn.addEventListener('click', toggleNavigation)
+
+function toggleNavigation() {
+    const toggle = navToggleBtn.getAttribute('aria-expanded') === "false" ? true : false;
+    navToggleBtn.setAttribute('aria-expanded', toggle)
+    navigation.setAttribute('aria-expanded', toggle)
+    console.log(toggle);
+}
